3986b1968666371c7162552338857cc3f9bc0bfe
Philip Fuerste <philip.fuerste@gmail.com>
2021-05-17 10:38:40 +0200

    main and function

--- input/screen_grab.py
+++ input/screen_grab.py
@@ -4,11 +4,17 @@ from PIL import ImageGrab
 from datetime import datetime
 from time import sleep
 
-conf = yaml.safe_load(open("input/config.yml"))
-while True:
+
+def take_screenshot():
     im = ImageGrab.grab()
     dt = datetime.now()
     fname = os.path.join("input", "data", "pic_{}.{}.png".format(
         dt.strftime("%H%M_%S"), dt.microsecond // 100000))
     im.save(fname, 'png')
-    sleep(conf["sleep_seconds"])
+
+
+if __name__ == "__main__":
+    conf = yaml.safe_load(open("input/config.yml"))
+    while True:
+        take_screenshot()
+        sleep(conf["sleep_seconds"])
