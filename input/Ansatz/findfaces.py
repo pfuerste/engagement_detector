b84f88bfac7b02c972034810191b04747d603881
alexander.bahr@uni-jena.de <alexander.bahr@uni-jena.de>
2021-06-08 23:34:48 +0200

    Pep8 Anpassung

--- input/Ansatz/findfaces.py
+++ input/Ansatz/findfaces.py
@@ -4,8 +4,8 @@ import os
 imagePath = r"E:\Users\Alexander\Desktop\Bilder\testung.png"
 os.chdir(r'E:\Users\Alexander\Desktop\Bilder\temp')
 
-image=face_recognition.load_image_file(imagePath)
-face_locations=face_recognition.face_locations(image)
+image = face_recognition.load_image_file(imagePath)
+face_locations = face_recognition.face_locations(image)
 
 print(face_locations)
 
