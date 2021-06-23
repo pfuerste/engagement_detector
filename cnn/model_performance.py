0ef860bb8121ed0f13cc1147516cb1836f67cd3e
Philip Fuerste <philip.fuerste@gmail.com>
2021-06-23 10:12:49 +0200

    moved analysis

--- cnn/model_performance.py
+++ cnn/model_performance.py
@@ -72,30 +72,9 @@ if __name__ == "__main__":
     test_df = dai.get_dataframe("Test")
     test_datagen = dai.get_flowing_datagen(dai.get_datagen(), test_df, "Test")
 
-    b, e, f, c = np.zeros((4)), np.zeros((4)), np.zeros((4)), np.zeros((4))
-    for label in test_datagen.labels:
-        b[label[0]] += 1
-        e[label[1]] += 1
-        c[label[2]] += 1
-        f[label[3]] += 1
-    print(b)
-    print(e)
-    print(c)
-    print(f)
-    pass
 #TODO Fix reihenfolge überall sonst
 #TODO  fix klassenimbalance
 #TODO größere bilder
 #TODO Trainiere neu
-    test_datagen = dai.reshaped_gen(test_datagen, 1)
-    all = np.zeros((4, 4))
-    for ind, (data, label) in enumerate(test_datagen):
-        for i, list in enumerate(label):
-            for j in list: 
-                all[i, j] += 1
-        if ind > 17000/32:
-            break
-    print(all)
-    # print("Got Datagen.")
 
     test_model(model, test_datagen, 17000/32)
