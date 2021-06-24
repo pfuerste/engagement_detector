c5a80cc07e368221853677f2d23ef028a4f0502a
Philip Fuerste <philip.fuerste@gmail.com>
2021-06-24 11:22:43 +0200

    testing inference time

--- cnn/inference.py
+++ cnn/inference.py
@@ -7,6 +7,7 @@ import numpy as np
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 from models import get_model, get_func_model
+import time
 
 
 def inference(model, img_list):
@@ -23,14 +24,18 @@ if __name__ == "__main__":
     model_dir = os.path.join(root, "cnn", "models")
     model = get_func_model()
     model.load_weights(os.path.join(model_dir, "checkpoints_func.hdf5"))
-    imgs = [os.path.join(data_root, "DataSet/FaceTest/500044/5000441001/5000441001104.jpg"),#]#,
-            os.path.join(data_root, "DataSet/FaceTest/500044/5000441001/5000441001121.jpg"),
-            os.path.join(data_root, "DataSet/FaceTest/940328/940328016/940328016233.jpg")]
+    imgs = [os.path.join(data_root, "DataSet/FaceTest32/500044/5000441001/5000441001104.jpg")]
+            # os.path.join(data_root, "DataSet/FaceTest32/500044/5000441001/5000441001121.jpg"),
+            # os.path.join(data_root, "DataSet/FaceTest32/940328/940328016/940328016233.jpg")]
     imgs = [plt.imread(path) for path in imgs]
     imgs = np.array(imgs)
     #print(imgs.shape)
     #print(inference(model, imgs))
+    start = time.perf_counter()
     preds = model.predict(imgs)
+    end = time.perf_counter()
+    print(f"3 images took {end-start} time(s).")
+
     print(len(preds))
     for pred in preds:
         print(pred, "\n")
