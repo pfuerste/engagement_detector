712828928a0ca1fdc10e846099efdb1ad50a8782
Philip Fuerste <philip.fuerste@gmail.com>
2021-06-28 10:38:21 +0200

    main loop planning

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from io import encodings_identity as ei
+from io import io_utils
+from io import face_extract as fe
+from io.utils import crop_bbs
+
+
+def main():
+    pass
+    # read config (developer info)
+    # load model
+    # open gui
+
+    # read from gui:
+    # lecture name (new or dropdown of old ones)
+    # input method (default: screenshot)
+    # inference intervals (?)
+
+    # loop while (not stop button pressed)
+    # get image
+    # find faces
+    # get encodings
+    # compare to last encodings
+    # inference
+    # match encodings & metrics
+    # calc intra-session metrics
+    # update gui
+
+    # save data
