b7360c1d0378dcda59aa9b6599414068abf809dd
Philip Fuerste <philip.fuerste@gmail.com>
2021-07-01 09:56:41 +0200

    draft of main loop

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ import face_recognition
 from io_utils import encodings_identity as ei
 from io_utils import persistence
 from io_utils import face_extract as fe
-from io_utils.utils import crop_bbs
+from io_utils.utils import crop_bbs, batchify
 from io_utils.screen_grab import screenshot
 from cnn.models import get_func_model
+
+
+def manage_encodings(inference_data, new_inferences, all_encodings, curr_encodings):
+    # Same faces found in current iteration than earlier
+    if all_encodings == curr_encodings:
+        assert len(new_inferences) == len(inference_data)
+        for i, inference in new_inferences:
+            inference_data[i].append(inference)
 
 
 def main():
@@ -29,58 +37,64 @@ def main():
     # read from gui:
     # lecture name (new or dropdown of old ones)
     # input method (default: screenshot)
+    # Performance_mode or not
     input_via = screenshot
-    # inference intervals (?)
+    # inference intervals (hardcoden)
 
-
-    # TODO ! list of found people with image, encodings and metric instead of many lists
     stop = False
-    last_encodings = list()
+    all_encodings = list()
+    inference_data = list()
     # loop while (not stop button pressed)
-    #while not stop:
-        # get image
-    for j in range(5):
+    # while not stop:
+    # get image
+    for j in range(2):
         start = time.perf_counter()
         imgs = input_via()
         # find faces
         face_locs = list()
         faces = list()
-        encodings = list()
+        curr_encodings = list()
         for img in imgs:
             img = np.array(img)
             locs = fe.face_recog_extract(img)
             face_locs.extend(locs)
+            # save encodings and faces
             for loc in locs:
                 face = crop_bbs(img, [loc])
                 faces.append(*face)
-        # get encodings
-        encode_start = time.perf_counter()
+                # Does not find encodings on just face, needs to take whole image
+                enc = face_recognition.face_encodings(img, [loc])
+                curr_encodings.append(enc[0])
 
-        for face in faces:
-            # TODO Does not take coordinates, which would speed it up:
-            enc = face_recognition.face_encodings(face)
-            #print(len(enc))
-            #print(type(enc[0]))
-
-            encodings.append(enc)
-        encode_end = time.perf_counter()
-        print(f"Encodings took {(encode_end-encode_start)} seconds.")
-
-            #  (0, face.shape[1], face.shape[0], 0)))
-        # compare to last encodings
-        compare_start = time.perf_counter()
-        if last_encodings:
-            for encoding in encodings:
-                print(len(last_encodings))
-                print(len(encoding))
-                ret = face_recognition.compare_faces(last_encodings, encoding)
-        #    print(ret)
-        compare_end = time.perf_counter()
-        print(f"Comparaisions took {(compare_end-compare_start)} seconds.")
-
-        last_encodings = encodings
         # inference
+        # Returns array of shape [num_targets=4, num_persons, num_classes=4]
+        probs = model.predict(batchify(faces))
+        probs = np.array(probs)
+        # Reshape to [num_targets=4, num_persons] by taking index of max in last dim
+        preds = probs.argmax(axis=2)
+        person_scores = preds.T
+
+        # probs = np.squeeze(probs, axis=-1)
+        # print(probs.shape)
+        # print(probs[0])
+        # print(probs[0, 0])
+        # print(probs[1, 0])
+
+        # probs_person = probs.T  # np.reshape(probs, (probs.))
+        # print(probs_person)
+        break
         # match encodings & metrics
+        compare_start = time.perf_counter()
+        if all_encodings:
+            for encoding in curr_encodings:
+                ret = face_recognition.compare_faces(all_encodings, encoding)
+                ret = [1 if x else 0 for x in ret]
+                # if sum(ret) == 1:
+
+        compare_end = time.perf_counter()
+        print(f"Comparisions took {(compare_end-compare_start)} seconds.")
+        # Update data-structures
+        all_encodings = curr_encodings
         # calc intra-session metrics
         # update gui
         end = time.perf_counter()
@@ -91,6 +105,6 @@ def main():
 if __name__ == "__main__":
     main()
     # start = time.perf_counter()
-    # preds = model.predict(imgs)
+    # probs = model.predict(imgs)
     # end = time.perf_counter()
     # print(f"3 images took {end-start} time(s).")
