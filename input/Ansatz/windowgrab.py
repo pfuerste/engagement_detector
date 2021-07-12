cdf588c97354ff79092e3ff24175d4fb8ad18ac7
Philip Fuerste <philip.fuerste@gmail.com>
2021-07-12 14:57:02 +0200

    pep8

--- input/Ansatz/windowgrab.py
+++ input/Ansatz/windowgrab.py
@@ -3,24 +3,23 @@ import os
 from PIL import Image
 import numpy as np
 
-sys.path.insert(0, os.path.abspath(r"E:\Users\Alexander\anaconda3\Lib\site-packages\win32\win32gui.pyd"))
-sys.path.insert(0, os.path.abspath(r"E:\Users\Alexander\anaconda3\Lib\site-packages\win32\win32ui.pyd"))
-sys.path.insert(0, os.path.abspath(r"E:\Users\Alexander\anaconda3\Lib\site-packages\win32\win32api.pyd"))
+# sys.path.insert(0, os.path.abspath(r"E:\Users\Alexander\anaconda3\Lib\site-packages\win32\win32gui.pyd"))
+# sys.path.insert(0, os.path.abspath(r"E:\Users\Alexander\anaconda3\Lib\site-packages\win32\win32ui.pyd"))
+# sys.path.insert(0, os.path.abspath(r"E:\Users\Alexander\anaconda3\Lib\site-packages\win32\win32api.pyd"))
 
 from win32api import GetSystemMetrics
 import win32gui
 import win32ui
 import win32.lib.win32con as win32con
 
-
 def background_screenshot(hwnd, width, height):
     wDC = win32gui.GetWindowDC(hwnd)
-    dcObj=win32ui.CreateDCFromHandle(wDC)
-    cDC=dcObj.CreateCompatibleDC()
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
     dataBitMap = win32ui.CreateBitmap()
     dataBitMap.CreateCompatibleBitmap(dcObj, width, height)
     cDC.SelectObject(dataBitMap)
-    cDC.BitBlt((0,0),(width, height) , dcObj, (0,0), win32con.SRCCOPY)
+    cDC.BitBlt((0, 0), (width, height), dcObj, (0, 0), win32con.SRCCOPY)
     bmpinfo = dataBitMap.GetInfo()
     bmpstr = dataBitMap.GetBitmapBits(True)
     im = Image.frombuffer(
@@ -30,31 +29,37 @@ def background_screenshot(hwnd, width, height):
     dcObj.DeleteDC()
     cDC.DeleteDC()
     win32gui.ReleaseDC(hwnd, wDC)
-    win32gui.DeleteObject(dataBitMap.GetHandle())    
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+    im.show()
     return np.array(im)
 
-def is_out_of_screen(x0,x1,y0,y1):
-    #i got a mistake on fullscreen sometimes(screen starts at -8 and not at 0)
-    width=GetSystemMetrics(0)
-    height=GetSystemMetrics(1)
-    if (x0<=width and x0>=0) and (x1<=width and x1>=0) and (y0<=width and y0>=0) and (y1<=height and y1>=0):
+
+def is_out_of_screen(x0, x1, y0, y1):
+    # i got a mistake on fullscreen sometimes(screen starts at -8 and not at 0)
+    width = GetSystemMetrics(0)
+    height = GetSystemMetrics(1)
+    if (x0 <= width and x0 >= 0) and (x1 <= width and x1 >= 0) and (
+            y0 <= width and y0 >= 0) and (y1 <= height and y1 >= 0):
         return True
     return False
 
+
 def windowgrab(name):
-    hwnd=win32.gui.FindWindow(None,name)
-    x0,y0,x1,y1=win32gui.GetWindowRect(hwnd)
-    #No Idea how to send it to GUI from here. Call it from Main?
-    return background_screenshot(hwnd,x1-abs(x0),y1-y0)
+    hwnd = win32.gui.FindWindow(None, name)
+    x0, y0, x1, y1 = win32gui.GetWindowRect(hwnd)
+    # No Idea how to send it to GUI from here. Call it from Main?
+    return background_screenshot(hwnd, x1 - abs(x0), y1 - y0)
 
-hwnd = win32gui.FindWindow(None, "Telegram")
-x0,y0,x1,y1=win32gui.GetWindowRect(hwnd)
-if is_out_of_screen(x0,y0,x1,y1)==False:
-    #gui_running.WindowWarning()
-    print(x0,y1,x1,y1)
-background_screenshot(hwnd, x1-x0, y1-y0)
 
-##needs to be in main
+if __name__ == "__main__":
+    hwnd = win32gui.FindWindow(None, "Telegram")
+    x0, y0, x1, y1 = win32gui.GetWindowRect(hwnd)
+    if is_out_of_screen(x0, y0, x1, y1) == False:
+        # gui_running.WindowWarning()
+        print(x0, y0, x1, y1)
+    background_screenshot(hwnd, x1 - x0, y1 - y0)
+
+# needs to be in main
 '''
 hwnd = win32gui.FindWindow(None, gui_start.WindowName)
 x0,y0,x1,y1=win32gui.GetWindowRect(hwnd)
